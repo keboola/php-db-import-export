8ecb3a09c2b277d96d4aa09d923b01160573ff3b
Martin Junger <martin.junger@keboola.com>
2022-07-18 16:30:18 +0200

    add new import option for Snowflake - table has types defined

--- src/Backend/Snowflake/SnowflakeImportOptions.php
+++ src/Backend/Snowflake/SnowflakeImportOptions.php
@@ -8,5 +8,34 @@ use Keboola\Db\ImportExport\ImportOptions;
 
 class SnowflakeImportOptions extends ImportOptions
 {
+    public const TABLE_TYPES_CAST = 'TABLE_HAS_TYPES_DEFINED';
+    public const TABLE_TYPES_PRESERVE = 'TABLE_TYPES_PRESERVE';
 
+    /** @var self::TABLE_TYPES_* */
+    private $castValueTypes;
+
+    /**
+     * @param string[] $convertEmptyValuesToNull
+     * @param self::TABLE_TYPES_* $castValueTypes
+     */
+    public function __construct(
+        array $convertEmptyValuesToNull = [],
+        bool $isIncremental = false,
+        bool $useTimestamp = false,
+        int $numberOfIgnoredLines = 0,
+        string $castValueTypes = self::TABLE_TYPES_PRESERVE
+    ) {
+        parent::__construct(
+            $convertEmptyValuesToNull,
+            $isIncremental,
+            $useTimestamp,
+            $numberOfIgnoredLines
+        );
+        $this->castValueTypes = $castValueTypes;
+    }
+
+    public function getCastValueTypes(): bool
+    {
+        return $this->castValueTypes === self::TABLE_TYPES_CAST;
+    }
 }
