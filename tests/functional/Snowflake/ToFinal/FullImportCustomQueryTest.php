cfb7fa44a3543b2d01259d1cc43a38ac33fd269d
Martin Junger <martin.junger@keboola.com>
2022-09-27 16:44:29 +0200

    add more tests

--- tests/functional/Snowflake/ToFinal/FullImportCustomQueryTest.php
+++ tests/functional/Snowflake/ToFinal/FullImportCustomQueryTest.php
@@ -152,6 +152,286 @@ class FullImportCustomQueryTest extends SnowflakeBaseTestCase
         $this->assertCount(7, $queries);
     }
 
+    public function testLoadFromS3ToFinalTableWithoutDedup(): void
+    {
+        $params = [
+            'sourceFiles' => [
+                'sourceFile1' => uniqid('sourceFile1'),
+            ],
+            'sourceS3Prefix' => uniqid('sourceS3Prefix'),
+            'sourceKey' => uniqid('sourceKey'),
+            'sourceSecret' => uniqid('sourceSecret'),
+            'sourceRegion' => uniqid('sourceRegion'),
+
+            'stageSchemaName' => uniqid('stageSchemaName'),
+            'stageTableName' => uniqid('__temp_stageTableName'),
+            'stageColumns' => [
+                'sourceCol1' => new SnowflakeColumn(
+                    uniqid('sourceCol1'),
+                    new Snowflake(Snowflake::TYPE_INT)
+                ),
+                'sourceCol2' => new SnowflakeColumn(
+                    uniqid('sourceCol2'),
+                    new Snowflake(Snowflake::TYPE_VARCHAR)
+                ),
+            ],
+
+            'destSchemaName' => uniqid('destSchemaName'),
+            'destTableName' => uniqid('destTableName'),
+            'destColumns' => [
+                'destCol1' => new SnowflakeColumn(
+                    uniqid('destColumn1'),
+                    new Snowflake(Snowflake::TYPE_INT)
+                ),
+                'destCol2' => new SnowflakeColumn(
+                    uniqid('destColumn2'),
+                    new Snowflake(Snowflake::TYPE_VARCHAR)
+                ),
+                'destColTimestamp' => new SnowflakeColumn(
+                    uniqid('_timestamp'),
+                    new Snowflake(Snowflake::TYPE_TIMESTAMP)
+                ),
+            ],
+        ];
+
+        $sourceColumnsNames = [];
+        /** @var ColumnInterface $column */
+        foreach ($params['stageColumns'] as $column) {
+            $sourceColumnsNames[] = $column->getColumnName();
+        }
+
+        $queries = [];
+
+        // mock connection
+        $conn = $this->createMock(Connection::class);
+        $conn->expects(self::atLeastOnce())->method('executeStatement')->willReturnCallback(
+            static function (...$values) use (&$queries) {
+                $queries[] = $values[0];
+                return 0;
+            }
+        );
+
+        // mock file source
+        $source = $this->createMock(Storage\S3\SourceFile::class);
+        $source->expects(self::atLeastOnce())->method('getCsvOptions')->willReturn(new CsvOptions());
+        $source->expects(self::atLeastOnce())->method('getManifestEntries')->willReturn($params['sourceFiles']);
+        $source->expects(self::atLeastOnce())->method('getColumnsNames')->willReturn($sourceColumnsNames);
+        // S3 scpecific
+        $source->expects(self::atLeastOnce())->method('getS3Prefix')->willReturn($params['sourceS3Prefix']);
+        $source->expects(self::atLeastOnce())->method('getKey')->willReturn($params['sourceKey']);
+        $source->expects(self::atLeastOnce())->method('getSecret')->willReturn($params['sourceSecret']);
+        $source->expects(self::atLeastOnce())->method('getRegion')->willReturn($params['sourceRegion']);
+
+        // fake staging table
+        $stagingTable = new SnowflakeTableDefinition(
+            $params['stageSchemaName'],
+            $params['stageTableName'],
+            true,
+            new ColumnCollection($params['stageColumns']),
+            []
+        );
+        // fake options
+        $options = new SnowflakeImportOptions(
+            [],
+            false,
+            false,
+            1
+        );
+        // fake destination
+        $destination = new SnowflakeTableDefinition(
+            $params['destSchemaName'],
+            $params['destTableName'],
+            false,
+            new ColumnCollection($params['destColumns']),
+            []
+        );
+
+        // mock importer
+        $importer = new ToStageImporter($conn);
+
+        // init query builder
+        $qb = new SnowflakeTableQueryBuilder();
+
+        // ACTION: create stage table
+        $conn->executeStatement(
+            $qb->getCreateTableCommandFromDefinition($stagingTable)
+        );
+
+        // ACTION: create final table
+        $conn->executeStatement(
+            $qb->getCreateTableCommandFromDefinition($destination)
+        );
+
+        // ACTION: import to stage table
+        $importState = $importer->importToStagingTable(
+            $source,
+            $stagingTable,
+            $options
+        );
+
+        // ACTION: import to final table
+        $toFinalTableImporter = new FullImporter($conn);
+        $result = $toFinalTableImporter->importToTable(
+            $stagingTable,
+            $destination,
+            $options,
+            $importState
+        );
+
+        // result
+        dump('=== queries');
+        foreach ($queries as $query) {
+//            dump($query);
+//            dump('---');
+            dump($this->replaceParamsInQuery($query, $params, '', ''));
+            dump('---------');
+        }
+
+        $this->assertCount(7, $queries);
+    }
+
+    public function testLoadFromAbsToFinalTableWithDedupWithSinglePK(): void
+    {
+        $sourceCol1Name = uniqid('sourceCol1');
+        $destCol1Name = uniqid('destColumn1');
+        $params = [
+            'sourceFiles' => [
+                'sourceFile1' => uniqid('sourceFile1'),
+            ],
+            'sourceContainerUrl' => uniqid('sourceContainerUrl'),
+            'sourceSasToken' => uniqid('sourceSasToken'),
+
+            'stageSchemaName' => uniqid('stageSchemaName'),
+            'stageTableName' => uniqid('__temp_stageTableName'),
+            'stageColumns' => [
+                'sourceCol1' => new SnowflakeColumn(
+                    $sourceCol1Name,
+                    new Snowflake(Snowflake::TYPE_INT)
+                ),
+                'sourceCol2' => new SnowflakeColumn(
+                    uniqid('sourceCol2'),
+                    new Snowflake(Snowflake::TYPE_VARCHAR)
+                ),
+            ],
+            'stagePrimaryKeys' => [
+                'sourceCol1' => $sourceCol1Name,
+            ],
+
+            'destSchemaName' => uniqid('destSchemaName'),
+            'destTableName' => uniqid('destTableName'),
+            'destColumns' => [
+                'destCol1' => new SnowflakeColumn(
+                    $destCol1Name,
+                    new Snowflake(Snowflake::TYPE_INT)
+                ),
+                'destCol2' => new SnowflakeColumn(
+                    uniqid('destColumn2'),
+                    new Snowflake(Snowflake::TYPE_VARCHAR)
+                ),
+                'destColTimestamp' => new SnowflakeColumn(
+                    uniqid('_timestamp'),
+                    new Snowflake(Snowflake::TYPE_TIMESTAMP)
+                ),
+            ],
+            'destPrimaryKeys' => [
+                'destPrimaryKey1' => $destCol1Name,
+            ]
+        ];
+
+        $sourceColumnsNames = [];
+        /** @var ColumnInterface $column */
+        foreach ($params['stageColumns'] as $column) {
+            $sourceColumnsNames[] = $column->getColumnName();
+        }
+
+        $queries = [];
+
+        // mock connection
+        $conn = $this->createMock(Connection::class);
+        $conn->expects(self::atLeastOnce())->method('executeStatement')->willReturnCallback(
+            static function (...$values) use (&$queries) {
+                $queries[] = $values[0];
+                return 0;
+            }
+        );
+
+        // mock file source
+        $source = $this->createMock(Storage\ABS\SourceFile::class);
+        $source->expects(self::atLeastOnce())->method('getCsvOptions')->willReturn(new CsvOptions());
+        $source->expects(self::atLeastOnce())->method('getManifestEntries')->willReturn($params['sourceFiles']);
+        $source->expects(self::atLeastOnce())->method('getColumnsNames')->willReturn($sourceColumnsNames);
+        // ABS specific
+        $source->expects(self::atLeastOnce())->method('getContainerUrl')->willReturn($params['sourceContainerUrl']);
+        $source->expects(self::atLeastOnce())->method('getSasToken')->willReturn($params['sourceSasToken']);
+
+        // fake staging table
+        $stagingTable = new SnowflakeTableDefinition(
+            $params['stageSchemaName'],
+            $params['stageTableName'],
+            true,
+            new ColumnCollection($params['stageColumns']),
+            $params['stagePrimaryKeys']
+        );
+        // fake options
+        $options = new SnowflakeImportOptions(
+            [],
+            false,
+            false,
+            1
+        );
+        // fake destination
+        $destination = new SnowflakeTableDefinition(
+            $params['destSchemaName'],
+            $params['destTableName'],
+            false,
+            new ColumnCollection($params['destColumns']),
+            $params['destPrimaryKeys']
+        );
+
+        // mock importer
+        $importer = new ToStageImporter($conn);
+
+        // init query builder
+        $qb = new SnowflakeTableQueryBuilder();
+
+        // ACTION: create stage table
+        $conn->executeStatement(
+            $qb->getCreateTableCommandFromDefinition($stagingTable)
+        );
+
+        // ACTION: create final table
+        $conn->executeStatement(
+            $qb->getCreateTableCommandFromDefinition($destination)
+        );
+
+        // ACTION: import to stage table
+        $importState = $importer->importToStagingTable(
+            $source,
+            $stagingTable,
+            $options
+        );
+
+        // ACTION: import to final table
+        $toFinalTableImporter = new FullImporter($conn);
+        $result = $toFinalTableImporter->importToTable(
+            $stagingTable,
+            $destination,
+            $options,
+            $importState
+        );
+
+        // result
+        dump('=== queries:');
+        foreach ($queries as $query) {
+//            dump($query);
+//            dump('---');
+            dump($this->replaceParamsInQuery($query, $params, '', ''));
+            dump('---------');
+        }
+
+        $this->assertCount(10, $queries);
+    }
+
     /**
      * Try to replace params back...
      */
